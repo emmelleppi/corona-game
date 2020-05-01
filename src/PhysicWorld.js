55914209b924c99eb4b1df423aeb54b63ecec8d7
emmelleppi <mlp.perego@gmail.com>
2020-05-01 13:10:23 +0200

    Adds initial trampoline

--- src/PhysicWorld.js
+++ src/PhysicWorld.js
@@ -1,15 +1,31 @@
 import React, { Suspense } from "react";
-import { Physics } from "use-cannon";
+import { Physics, useBox } from "use-cannon";
 
 import FirstPersonCamera from "./FirstPersonCamera";
 import Map from "./Map";
 import CoronaManager from './CoronaManager'
+import { COLLISION_GROUP } from "./store";
+
+function Trampoline() {
+  const [planeBody] = useBox(() => ({
+    args: [0.1, 0.1, 0.1],
+    rotation: [0, 0, 0],
+    position: [0, 29, 5],
+    type: "Static",
+    collisionFilterGroup: COLLISION_GROUP.TILES,
+  }))
+
+  return <mesh ref={planeBody} />
+}
 
 function PhysicWorld() {
 
+
+
   return (
     <Physics gravity={[0, -20, 0]} tolerance={0.0001} allowSleep={false} >
-      <FirstPersonCamera position={[0, 30, 0]} />
+      <FirstPersonCamera position={[0, 30, 5]} />
+      <Trampoline />
       <Suspense fallback={null}>
         <Map />
         <CoronaManager />
