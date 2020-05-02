5c28ed2b5bea9baeca3ed7b567170ab313c20e9f
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-05-02 15:06:23 +0200

    Changes corona on title screen after 10 seconds

--- src/GestureHandler.js
+++ src/GestureHandler.js
@@ -8,107 +8,117 @@ import { PointerLockControls } from "./PointerLockControls";
 import { useInteraction, gameApi, useGame, playerApi, useCorona } from "./store";
 
 function PreGameMode() {
-    const { camera } = useThree();
+  const { camera } = useThree();
 
-    const coronas = useCorona(s => s.coronas)
-    const corona = useMemo(() => coronas?.[0]?.store?.[1]?.getState().ref, [coronas])
+  const coronas = useCorona(s => s.coronas)
 
-    useFrame(() => {
-        if (corona?.current) {
-            const { orientation } = coronas?.[0]?.store?.[1]?.getState()
-            const { x, y, z } = corona.current.position
+  const [index, setIndex] = React.useState(0)
+  const corona = useMemo(() => coronas?.[index]?.store?.[1]?.getState(), [coronas, index])
 
-            const lookAtVector = new THREE.Vector3(x - orientation.x, y, z - orientation.z);
+  let t = useRef(0)
+  useFrame(({ clock }) => {
+    t.current += clock.getDelta() * 1000
 
-            camera.position.lerp(lookAtVector, 0.1);
-            camera.lookAt(corona.current.position);
-        }
-    })
-    
-    return null
+    if (t.current > 15) {
+      t.current = 0;
+      setIndex(index => (index + 1) % coronas.length)
+    }
+
+    if (corona?.ref.current) {
+      const { orientation } = corona
+      const { x, y, z } = corona.ref.current.position
+
+      const lookAtVector = new THREE.Vector3(x - orientation.x, y + 0.5, z - orientation.z);
+
+      camera.position.lerp(lookAtVector, 0.2);
+      camera.lookAt(x, y, z);
+    }
+  })
+
+  return null
 }
 
 function GestureHandler(props) {
-    const { children } = props
+  const { children } = props
 
-    const { scene, setDefaultCamera, size } = useThree();
+  const { scene, setDefaultCamera, size } = useThree();
 
-    const controls = useRef();
-    const camera = useRef();
+  const controls = useRef();
+  const camera = useRef();
 
-    const playerBody = useMemo(() => playerApi.getState().playerBody, [playerApi])
+  const playerBody = useMemo(() => playerApi.getState().playerBody, [playerApi])
 
-    const { isStarted: isGameStarted, isStartAnimation } = useGame(s => s)
-    const { boost } = useInteraction(s => s)
-    const { onDocumentKeyDown, onDocumentKeyUp } = useInteraction(s => s.actions)
+  const { isStarted: isGameStarted, isStartAnimation } = useGame(s => s)
+  const { boost } = useInteraction(s => s)
+  const { onDocumentKeyDown, onDocumentKeyUp } = useInteraction(s => s.actions)
 
-    const spring = useSpring({ fov: boost ? 100 : 70, config: config.molasses })
-    
-    const lockPointerLock = useCallback(
-        function lockPointerLock() {
-            if (controls.current) {
-                const { initGame } = gameApi.getState()
-                initGame()
-                controls.current.lock();
-            }
-        },
-        [controls]
-    );
+  const spring = useSpring({ fov: boost ? 100 : 70, config: config.molasses })
 
-    useEffect(() => void (setDefaultCamera(camera.current)), [setDefaultCamera, camera])
+  const lockPointerLock = useCallback(
+    function lockPointerLock() {
+      if (controls.current) {
+        const { initGame } = gameApi.getState()
+        initGame()
+        controls.current.lock();
+      }
+    },
+    [controls]
+  );
 
-    useEffect(() => {
-        const canvas = document.getElementsByTagName("canvas")[0];
-        controls.current = new PointerLockControls(camera.current, canvas);
-        canvas.addEventListener("click", lockPointerLock, false);
-    
-        const obj = controls.current.getObject();
-        scene.add(obj);
+  useEffect(() => void (setDefaultCamera(camera.current)), [setDefaultCamera, camera])
 
-        return () => {
-            canvas.removeEventListener("click", lockPointerLock);
-            controls.current.unlock()
-            scene.remove(obj);
-        }
-    }, [controls,lockPointerLock, scene])
+  useEffect(() => {
+    const canvas = document.getElementsByTagName("canvas")[0];
+    controls.current = new PointerLockControls(camera.current, canvas);
+    canvas.addEventListener("click", lockPointerLock, false);
 
-    useEffect(() => {
-        document.addEventListener("keydown", onDocumentKeyDown, false);
-        document.addEventListener("keyup", onDocumentKeyUp, false);
-    
-        return () => {
-            document.removeEventListener("keydown", onDocumentKeyDown);
-            document.removeEventListener("keyup", onDocumentKeyUp);
-        };
-    }, [onDocumentKeyDown, onDocumentKeyUp]);
+    const obj = controls.current.getObject();
+    scene.add(obj);
 
-    useEffect(() => void (isStartAnimation && camera.current.lookAt(0, -1, -0.5)), [isStartAnimation, camera])
+    return () => {
+      canvas.removeEventListener("click", lockPointerLock);
+      controls.current.unlock()
+      scene.remove(obj);
+    }
+  }, [controls, lockPointerLock, scene])
 
-    useFrame(() => {
-        if (playerBody.current) {
-            if (isGameStarted) {
-                camera.current.position.copy(playerBody.current.position)
-            }
-            if (isStartAnimation) {
-                camera.current.position.lerp(playerBody.current.position, 0.1)
-            }
-        }
-    })
+  useEffect(() => {
+    document.addEventListener("keydown", onDocumentKeyDown, false);
+    document.addEventListener("keyup", onDocumentKeyUp, false);
 
-    return(
-        <a.perspectiveCamera
-            ref={camera}
-            args={[60, size.width / size.height, .1, 300]}
-            {...spring}
-            onUpdate={camera => {
-                camera.aspect = size.width / size.height
-                camera.updateProjectionMatrix()
-            }}
-        >
-            {!isStartAnimation && <PreGameMode />}
-            {isGameStarted && children}
-        </a.perspectiveCamera>
-    )
+    return () => {
+      document.removeEventListener("keydown", onDocumentKeyDown);
+      document.removeEventListener("keyup", onDocumentKeyUp);
+    };
+  }, [onDocumentKeyDown, onDocumentKeyUp]);
+
+  useEffect(() => void (isStartAnimation && camera.current.lookAt(0, -1, -0.5)), [isStartAnimation, camera])
+
+  useFrame(() => {
+    if (playerBody.current) {
+      if (isGameStarted) {
+        camera.current.position.copy(playerBody.current.position)
+      }
+      if (isStartAnimation) {
+        camera.current.position.lerp(playerBody.current.position, 0.1)
+      }
+    }
+  })
+
+  return (
+    <a.perspectiveCamera
+      ref={camera}
+      args={[60, size.width / size.height, .1, 300]}
+      {...spring}
+      onUpdate={camera => {
+        camera.aspect = size.width / size.height
+        camera.updateProjectionMatrix()
+      }}
+    >
+      {!isStartAnimation && <PreGameMode />}
+      {isGameStarted && children}
+    </a.perspectiveCamera>
+  )
 }
 
 export default GestureHandler
