c39344d2f4143e5264c7a0dce4a106d330ae95d2
emmelleppi <mlp.perego@gmail.com>
2020-05-19 22:48:17 +0200

    Removes unused three fix

--- src/utility/fix.js
+++
@@ -1,6 +0,0 @@
-import * as THREECJS from "three";
-import * as THREE from "three/build/three.module";
-
-// This hacks around threejs broken module resolution in CJS environments
-// See: https://github.com/mrdoob/three.js/issues/17220
-Object.assign(THREE, THREECJS);
